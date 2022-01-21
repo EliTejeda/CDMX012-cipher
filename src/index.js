eef5c196a80c5bb9125139ac0995567d7bc75812
EliTejeda <tejeda.eliza@gmail.com>
2022-01-21 13:26:27 -0600

    started

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+//funciones que deben de suceder
+//charCodeAt()
+//ACII
+
+
+//funciones para los botones
+let btncifrar = document.getElementById("btncifrar") // llamando al botón de HTML
+btncifrar.addEventListener("click", cifrar) // Cuando le piquen al botón, realiza la función cifrar
+
+let btndecifrar = document.getElementById("btndecifrar") // llamando al botón de HTML
+btndecifrar.addEventListener("click", decifrar) // Cuando le piquen al botón, realiza la función decifrar
+
+function cifrar() { 
+    let mensaje = document.getElementById("txtentrada").value // Agarra el valor de "txtentrada" que metió el usuario
+    let offset = document.getElementById("offset").value // Agarra el valor de "offset" que metió el usuario
+    console.log(offset, mensaje) // Muestra esto en la consola
+}
+    
+function decifrar() {
+    let mensaje = document.getElementById("txtentrada").value 
+    let offset = document.getElementById("offset").value
+    console.log(offset, mensaje)
+}
+
+//document.getElementById("txtresultado").innerHTML = "Hola Eli"
+
+//let btnlimpiar = document.getElementById("btnlimpiar")
+//btnlimpiar.addEventListener("click", capturarmsj)
